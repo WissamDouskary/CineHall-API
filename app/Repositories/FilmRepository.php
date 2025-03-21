0b029e7143123cf7874c5a17540cf44f20c94f65
WissamDouskary <douskary.wissam@student.youcode.ma>
2025-03-21 00:12:36 +0000

    create film repository

---
+++ app/Repositories/FilmRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Film;
+use App\Repositories\contract\FilmRepositoryInterface;
+
+class FilmRepository implements FilmRepositoryInterface
+{
+    public function store(array $data) : Film {
+        return Film::create($data);
+    }
+
+    public function update(array $data, int $id) : int {
+        $film = Film::findOrFail($id);
+
+        return $film->update($data);
+    }
+
+    public function destroy(int $id) : int {
+        $film = Film::findOrFail($id);
+
+        return $film->delete();
+    }
+
+}
