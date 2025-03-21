c2b1e1457aef23b23d8bbcc4a5a81075d162196b
WissamDouskary <douskary.wissam@student.youcode.ma>
2025-03-21 14:32:04 +0000

    create session repository interface

---
+++ app/Repositories/contract/SessionRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories\contract;
+
+interface SessionRepositoryInterface
+{
+    public function getall();
+    public function store(array $data);
+    public function update(array $data, int $id);
+    public function destroy(int $id);
+}
