805b12c5e54830b6ccad04b53b062fade24cb4a2
WissamDouskary <wissamdouskary99@gmail.com>
2025-03-21 00:12:48 +0000

    create film interface repository

---
+++ app/Repositories/contract/FilmRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories\contract;
+
+use App\Models\Film;
+
+interface FilmRepositoryInterface
+{
+    public function store(array $data) : Film;
+    public function update(array $data, int $id) : int;
+    public function destroy(int $id) : int;
+}
