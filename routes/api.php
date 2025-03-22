cabd6bef039000a3cfccac7c6364fe7be50674bb
WissamDouskary <douskary.wissam@student.youcode.ma>
2025-03-22 14:55:12 +0000

    create session route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SessionController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\FilmController;
 use Illuminate\Http\Request;
@@ -28,4 +29,7 @@ Route::group(['middleware' => ['auth:api']], function() {
     Route::post('/film', [FilmController::class, 'store']);
     Route::put('film/{id}', [FilmController::class, 'update']);
     Route::delete('film/{id}', [FilmController::class, 'destroy']);
+
+    //session
+    Route::post('/session', [SessionController::class, 'store']);
 });
