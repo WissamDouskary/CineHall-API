9f8ae8d0bab6c972758637acce30798928b9d84e
WissamDouskary <wissamdouskary99@gmail.com>
2025-03-23 23:42:02 +0000

    bind seat interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,15 +3,20 @@
 namespace App\Providers;
 
 use App\Repositories\contract\ReservationRepositoryInterface;
+use App\Repositories\contract\RoomRepositoryInterface;
+use App\Repositories\contract\SeatRepositoryInterface;
 use App\Repositories\contract\SessionRepositoryInterface;
 use App\Repositories\contract\UserRepositoryInterface;
 use App\Repositories\ReservationRepository;
+use App\Repositories\SeatRepository;
 use App\Repositories\SessionRepository;
 use App\Repositories\UserRepository;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\contract\FilmRepositoryInterface;
 use App\Repositories\FilmRepository;
+use App\Repositories\RoomRepository;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(FilmRepositoryInterface::class, FilmRepository::class);
         $this->app->bind(SessionRepositoryInterface::class, SessionRepository::class);
         $this->app->bind(ReservationRepositoryInterface::class, ReservationRepository::class);
+        $this->app->bind(RoomRepositoryInterface::class, RoomRepository::class);
+        $this->app->bind(SeatRepositoryInterface::class, SeatRepository::class);
 
         $this->app->bind(UserService::class, function ($app) {
             return new UserService($app->make(UserRepositoryInterface::class));
