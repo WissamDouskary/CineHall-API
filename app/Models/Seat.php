7e63ad1927085b72848456eb15f44844a4a1819f
WissamDouskary <douskary.wissam@student.youcode.ma>
2025-03-23 23:42:41 +0000

    edit seat model and controller

---
+++ app/Models/Seat.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Room;
+
+class Seat extends Model
+{
+    protected $fillable = ['room_id', 'seat_number'];
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+}
