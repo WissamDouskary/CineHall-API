46f43c20a0c0065ee82a74e440abb1b70db6b520
WissamDouskary <douskary.wissam@student.youcode.ma>
2025-03-25 13:37:51 +0000

    upgrade getall methode to show room with it seats and show is seats is reserved of not and session that be in this room with film id

--- app/Repositories/RoomRepository.php
+++ app/Repositories/RoomRepository.php
@@ -4,13 +4,33 @@ namespace App\Repositories;
 
 use App\Models\Room;
 use App\Repositories\contract\RoomRepositoryInterface;
+use App\Models\Reservation;
 
 class RoomRepository implements RoomRepositoryInterface
 {
     public function create($data){
         return Room::create($data);
     }
+
     public function getall(){
-        return Room::all();
+        $rooms = Room::with('seats')->with('sessions')->get();
+
+        foreach ($rooms as $room) {
+            foreach ($room->seats as $seat) {
+                $session = $room->sessions()->whereNotNull('room_id')->first();
+
+                if ($session) {
+                    $reservation = Reservation::where('seat_id', $seat->id)
+                        ->where('session_id', $session->id)
+                        ->first();
+
+                    $seat->is_reserved = $reservation ? true : false;
+                } else {
+                    $seat->is_reserved = false;
+                }
+            }
+        }
+
+        return $rooms;
     }
 }
