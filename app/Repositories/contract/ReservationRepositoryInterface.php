4adf89bcf6306362a0dc9f34de2ff0cc0a3cc7a3
WissamDouskary <wissamdouskary99@gmail.com>
2025-03-25 20:52:55 +0000

    create modify methode in signature in Reservation Repository Interface

--- app/Repositories/contract/ReservationRepositoryInterface.php
+++ app/Repositories/contract/ReservationRepositoryInterface.php
@@ -5,4 +5,5 @@ namespace App\Repositories\contract;
 interface ReservationRepositoryInterface
 {
     public function reserveSeat(int $userId, int $sessionId, int $seatId, int $seatsCount);
+    public function modify(int $reservationId, $data);
 }
