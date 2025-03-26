231816d0c9c5d94cc97dfa1811ec310166bf3237
WissamDouskary <douskary.wissam@student.youcode.ma>
2025-03-26 10:39:01 +0000

    create destroy reservation repository interface methode signature

--- app/Repositories/contract/ReservationRepositoryInterface.php
+++ app/Repositories/contract/ReservationRepositoryInterface.php
@@ -6,4 +6,5 @@ interface ReservationRepositoryInterface
 {
     public function reserveSeat(int $userId, int $sessionId, int $seatId, int $seatsCount);
     public function modify(int $reservationId, $data);
+    public function cancel(int $reservationId);
 }
