1dc1a25707b14cb2dbb5474c3b7935a80f60e1a8
WissamDouskary <wissamdouskary99@gmail.com>
2025-03-27 11:35:59 +0000

    create ticket table migration content

--- database/migrations/2025_03_26_233723_create_tickets_table.php
+++ database/migrations/2025_03_26_233723_create_tickets_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTicketsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('reservation_id')->constrained()->onDelete('cascade'); // Foreign key to Reservation
+            $table->string('film');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->string('seat');
+            $table->text('qr_code');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('tickets');
     }
-};
+}
